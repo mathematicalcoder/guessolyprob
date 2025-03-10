07002ff1b783cffc09592d0b8a940ab0d06cf3d2
mathemolympiad <kaiserchan1023@gmail.com>
2025-03-10 10:01:07 +0800

    update!!!!

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const problems = [
     {
-        "problem": "Turbo the snail plays a game on a board with $2024$ rows and $2023$ columns. There are hidden monsters in $2022$ of the cells. Initially, Turbo does not know where any of the monsters are, but he knows that there is exactly one monster in each row except the first row and the last row, and that each column contains at most one monster.<br><br>Turbo makes a series of attempts to go from the first row to the last row. On each attempt, he chooses to start on any cell in the first row, then repeatedly moves to an adjacent cell sharing a common side. (He is allowed to return to a previously visited cell.) If he reaches a cell with a monster, his attempt ends and he is transported back to the first row to start a new attempt. The monsters do not move, and Turbo remembers whether or not each cell he has visited contains a monster. If he reaches any cell in the last row, his attempt ends and the game is over.<br><br>Determine the minimum value of $n$ for which Turbo has a strategy that guarantees reaching the last row on the $n$-th attempt or earlier, regardless of the locations of the monsters.",
+        "problem": "turboTheSnail1.png",
         "contest": "IMO",
         "iteration": "2024",
         "problemNo": "5"
     },
     {
-        "problem": "Tasyo is thinking of a two digit number. He tells Basilio the tens digit and Crispin the ones digit, and challenges them to guess the number. Their conversation goes as follows:<br>Crispin: I know the number is not prime.<br>Tasyo: Correct, it is divisible by 3.<br>Basilio: Now I know what the number is.<br>What digit did Tasyo tell Crispin?",
+        "problem": "noli.png",
         "contest": "PMO Qualifying",
         "iteration": "2024",
         "problemNo": "III.2"
-    }
+    },
+    {
+        "problem": "eisenBlackboard.png",
+        "contest": "PMO Areas",
+        "iteration": "2024",
+        "problemNo": "II.1"
+    },
+    {
+        "problem": "grasshopper.png",
+        "contest": "Putnam",
+        "iteration": "2021",
+        "problemNo": "A1"
+    },
+    
 ]
 
 MathJax = {
@@ -26,7 +39,7 @@ function giveProblem() {
     document.getElementById("iteration").disabled = false;
     document.getElementById("problemNo").disabled = false;
     index = Math.floor(Math.random() * problems.length);
-    document.getElementById("problem").innerHTML = problems[index]["problem"];
+    document.getElementById("problem").setAttribute('src', "images/" + problems[index]["problem"]);
 
     MathJax.startup.promise.then(() => {
         MathJax.typeset();
